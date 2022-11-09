fb028753e52f5a85900b8c29b4ec25a8ec03c17c
denisgorlachev <117777771+denisgorlachev@users.noreply.github.com>
2022-11-09 19:46:35 +0200

    Добавьте файлы проекта.

---
+++ лаба 3.3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Горлачев 
+Console.WriteLine("Введите число n: ");
+double n = double.Parse(Console.ReadLine());
+Console.WriteLine("Введите число x: ");
+double x = double.Parse(Console.ReadLine());
+double s = 0;
+double z = 1;
+for (int i = 0; i < n; i += 1)
+{
+    z = 2;
+    s = Math.Pow(x, z)*Math.Cos(z * Math.PI / 3) / 1;
+
+}
+Console.WriteLine("Ответ: " + s);
